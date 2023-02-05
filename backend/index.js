8d8d27f5750003370e83e3380e9da0af26acf6c6
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-05 11:16:15 +0700

    upd: change default route api, add port, config middleware, etc

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const app = express();
+const PORT = process.env.SERVER_PORT || 8000;
 
 const user = require('./routes/user');
+// const tipe_kamar = require('./routes/tipe_kamar');
+// const kamar = require('./routes/kamar');
 
 app.use(cors());
-app.use('/hotel/user', user);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8000, () => console.log('Server started on http://localhost:8000 🚀'));
+app.use('/api/v1/hotel/user', user);
+// app.use('/api/v1/hotel/room-type', tipe_kamar);
+// app.use('/api/v1/hotel/room', kamar);
+// app.use('/api/v1/hotel/booking', booking);
+
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT} 🚀`));
