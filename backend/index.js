57aa11c873ad9ee91db61295e592219c281d67e1
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-06 07:37:29 +0700

    upd: declare routes on index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,8 @@ const PORT = process.env.SERVER_PORT || 8000;
 
 const user = require('./routes/user');
 const tipe_kamar = require('./routes/tipe_kamar');
-// const kamar = require('./routes/kamar');
+const kamar = require('./routes/kamar');
+const pemesanan = require('./routes/pemesanan');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/hotel/user', user);
 app.use('/api/v1/hotel/room-type', tipe_kamar);
-// app.use('/api/v1/hotel/room', kamar);
-// app.use('/api/v1/hotel/booking', booking);
+app.use('/api/v1/hotel/room', kamar);
+app.use('/api/v1/hotel/booking', pemesanan);
 
 app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT} 🚀`));
