186ffbda0715503f456d7fb336009c3c1fdeb55e
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-06 13:17:51 +0700

    upd: change image facilities

--- frontend/src/components/Containers/Facilities/Facilities.tsx
+++ frontend/src/components/Containers/Facilities/Facilities.tsx
@@ -68,7 +68,7 @@ const ContainerFacilities: FC = () => {
                     className="group h-48 md:h-80 flex items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
                   >
                     <img
-                      src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+                      src="/assets/img/hotel_room.jpg"
                       loading="lazy"
                       alt="Photo by Minh Pham"
                       className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
@@ -77,7 +77,7 @@ const ContainerFacilities: FC = () => {
                     <div className="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
 
                     <span className="inline-block text-white text-sm md:text-lg relative ml-4 md:ml-5 mb-3">
-                      VR
+                      Kamar
                     </span>
                   </a>
                   {/* image end */}
@@ -88,7 +88,7 @@ const ContainerFacilities: FC = () => {
                     className="group h-48 md:h-80 md:col-span-2 flex items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
                   >
                     <img
-                      src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+                      src="/assets/img/hotel_gym.jpeg"
                       loading="lazy"
                       alt="Photo by Magicle"
                       className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
@@ -96,7 +96,7 @@ const ContainerFacilities: FC = () => {
                     <div className="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
 
                     <span className="inline-block text-white text-sm md:text-lg relative ml-4 md:ml-5 mb-3">
-                      Tech
+                      Gym
                     </span>
                   </a>
                   {/* image end */}
@@ -107,7 +107,7 @@ const ContainerFacilities: FC = () => {
                     className="group h-48 md:h-80 md:col-span-2 flex items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
                   >
                     <img
-                      src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=1000"
+                      src="/assets/img/hotel_pool.jpeg"
                       loading="lazy"
                       alt="Photo by Martin Sanchez"
                       className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
@@ -126,7 +126,7 @@ const ContainerFacilities: FC = () => {
                     className="group h-48 md:h-80 flex items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
                   >
                     <img
-                      src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
+                      src="/assets/img/hotel_dining.jpeg"
                       loading="lazy"
                       alt="Photo by Lorenzo Herrera"
                       className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
