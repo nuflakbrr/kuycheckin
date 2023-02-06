99c1e2fe207768c686f6f59510b255b6a24c6465
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-06 07:32:17 +0700

    feat: room routes

---
+++ backend/routes/kamar.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const auth = require('../middleware/auth');
+const tipe_kamar = require('../models/index').tipe_kamar;
+const kamar = require('../models/index').kamar;
+
+const app = express();
+
+/**
+ * @apiRoutes {get} /hotel/room/
+ * @apiName GetAllRoom
+ * @apiGroup Room
+ * @apiDescription Get all room data
+ */
+app.get('/', async (req, res) => {
+  await kamar.findAll({ include: ['tipe_kamar'] })
+  .then(result => res.json({ data: result }))
+  .catch(error => res.json({ message: error.message }))
+});
+
+/**
+ * @apiRoutes {get} /hotel/room/:id
+ * @apiName GetRoomById
+ * @apiGroup Room
+ * @apiDescription Get room data by id
+ */
+app.get('/:id', async (req, res) => {
+  let params = { id_kamar: req.params.id };
+
+  await kamar.findOne({ where: params, include: ['tipe_kamar'] })
+  .then(result => res.json({ data: result }))
+  .catch(error => res.json({ message: error.message }))
+});
+
+/**
+ * @apiRoutes {post} /hotel/room/
+ * @apiName PostRoom
+ * @apiGroup Room
+ * @apiDescription Insert room data
+ */
+app.post('/', auth, async (req, res) => {
+  let data = {
+    nomor_kamar: req.body.nomor_kamar,
+    id_tipe_kamar: req.body.id_tipe_kamar,
+  }
+
+  await kamar.create(data)
+  .then(result => res.json({ success: 1, message: "Data has been inserted", data: result }))
+  .catch(error => res.json({ message: error.message }))
+});
+
+/**
+ * @apiRoutes {put} /hotel/room/:id
+ * @apiName PutRoom
+ * @apiGroup Room
+ * @apiDescription Update room data
+ */
+app.put('/:id', auth, async (req, res) => {
+  let params = { id_kamar: req.params.id };
+  let data = {
+    nomor_kamar: req.body.nomor_kamar,
+    id_tipe_kamar: req.body.id_tipe_kamar,
+  }
+
+  await kamar.update(data, { where: params })
+  .then(result => res.json({ success: 1, message: "Data has been updated" }))
+  .catch(error => res.json({ message: error.message }))
+});
+
+/**
+ * @apiRoutes {delete} /hotel/room/:id
+ * @apiName DeleteRoom
+ * @apiGroup Room
+ * @apiDescription Delete room data
+ */
+app.delete('/:id', auth, async (req, res) => {
+  let params = { id_kamar: req.params.id };
+
+  await kamar.destroy({ where: params })
+  .then(result => res.json({ success: 1, message: "Data has been deleted" }))
+  .catch(error => res.json({ message: error.message }))
+});
+
+module.exports = app;
