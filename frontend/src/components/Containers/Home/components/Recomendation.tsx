63dc401cb4aca2311787d359dface11232f2383c
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-07 20:04:23 +0700

    upd: add subtext

--- frontend/src/components/Containers/Home/components/Recomendation.tsx
+++ frontend/src/components/Containers/Home/components/Recomendation.tsx
@@ -11,10 +11,15 @@ const RecomendationSection: FC = () => {
       <div className="max-w-7xl mx-auto">
         <div className="container">
           <div className="flex items-center justify-between">
-            <div className="w-full px-4">
-              <h1 className="max-w-sm lg:max-w-none font-primary font-semibold text-2xl lg:text-3xl mb-10">
-                Rekomendasi Kamar Untuk Anda
+            <div className="w-full px-4 mb-10">
+              <h1 className="max-w-sm lg:max-w-none font-primary font-semibold text-2xl lg:text-3xl mb-2">
+                Rekomendasi Kamar Terbaik untuk Anda
               </h1>
+
+              <p className="max-w-screen-md text-gray-500 xl:text-lg leading-relaxed">
+                Tersedia berbagai macam kamar yang dapat Anda pilih sesuai
+                kebutuhan Anda.
+              </p>
             </div>
 
             <div className="w-28 px-4">
