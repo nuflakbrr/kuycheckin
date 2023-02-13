664fe1d9667153befc1145d5d522060bacf71442
Naufal Akbar <kbrnugroho@gmail.com>
2023-02-13 19:14:01 +0700

    feat: update data user

---
+++ frontend/src/pages/admin/user/edit/[id].tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainerEditUser from '../../../../components/Containers/Admin/User/components/EditUser';
+
+const EditUser: FC = () => {
+  return <ContainerEditUser />;
+};
+
+export default EditUser;
