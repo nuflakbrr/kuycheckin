446a9bb9f0facee154292abadc26a5e9435b76ca
Naufal Akbar <kbrnugroho@gmail.com>
2023-03-03 23:10:21 +0700

    feat: create page and sidebar customer

---
+++ frontend/src/pages/customer/dashboard.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainerDashboard from '@/components/Containers/Dashboard/Dashboard';
+
+const DashboardCustomer: FC = () => {
+  return <ContainerDashboard />;
+};
+
+export default DashboardCustomer;
